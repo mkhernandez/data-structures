c5a0cc480dfd3616f8893b9282e939b095cdc352
Mark Hernandez <markkimhernandez@gmail.com>
2023-09-07 19:43:14 -0500

    Finished topKFrequentElements

--- arrays/topKFrequentElements.js
+++ arrays/topKFrequentElements.js
@@ -13,8 +13,26 @@
 //at which the number of times it occurs
 
 var topKFrequent = function(nums, k) {
-    let map = new Map();
-    let arr = new Array(nums.length +1).fill(0);
+    let map = new Map();//map the value and frequency
+    let arr = new Array(nums.length +1).fill(0);//bucket array
+    let result = [];//array to hold the result
+
+    //loop through the nums array and map the frequency
+    for(const num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+    //loop through the map and add to corresponding index the 
+    //frequency
+    for(let [num, freq] of map) {
+        arr[freq] = (arr[freq] || new Set()).add(num);
+    }
+    //loop through the bucket array(arr) and find the ones with the most frequency
+    //and add them to the result array
+    for(let i = arr.length-1; i >= 0;i--) {
+        if(arr[i]) result.push(...arr[i]);
+        if(result.length === k) break;
+    }
+    return result;
 };
 
-topKFrequent([1, 1, 1, 2, 2, 3], 2);
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
